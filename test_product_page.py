873406d691e0eac9e656f5618bd73c78193eceed
Verteletsky Roman <verteletsky@bitrix24.ru>
2022-08-03 23:56:06 +0300

    make class for two test and added mark

--- test_product_page.py
+++ test_product_page.py
@@ -80,3 +80,25 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     open_basket.should_be_empty_basket()
     open_basket.should_be_message_empty_basket()
 
+
+# @pytest.mark.login
+# class TestLoginFromProductPage():
+#     @pytest.fixture(scope="function", autouse=True)
+#     def setup(self):
+#         self.product = ProductFactory(title="Best book created by robot")
+#         # создаем по апи
+#         self.link = self.product.link
+#         yield
+#         # после этого ключевого слова начинается teardown
+#         # выполнится после каждого теста в классе
+#         # удаляем те данные, которые мы создали
+#         self.product.delete()
+#
+#     def test_guest_can_go_to_login_page_from_product_page(self, browser):
+#         page = ProductPage(browser, self.link)
+#         # дальше обычная реализация теста
+#
+#     def test_guest_should_see_login_link(self, browser):
+#         page = ProductPage(browser, self.link)
+#         # дальше обычная реализация теста
+
